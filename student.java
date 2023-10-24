2cb082180bf0f4563ddeb1273714483477ed8571
JoshuaMatthewBAgpalza <148180885+JoshuaMatthewBAgpalza@users.noreply.github.com>
2023-10-24 13:00:29 +0800

    Add files via upload

---
+++ student.java
@@ -0,0 +1,8 @@
+package student
+class StudentInterface{
+public static void main(String a[]){
+Student sOne = new Student();
+sOne.setFirstName("Joshua Matthew");
+sOne.setMiddle("B");
+sOne.setLastName("Agpalza");
+sOne
